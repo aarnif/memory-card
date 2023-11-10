2c261f489580149c8e36e96252acdedd87d614e8
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2023-11-10 18:55:47 +0200

    Rename variables and create helper functions for game situation checks

--- src/App.jsx
+++ src/App.jsx
@@ -12,46 +12,50 @@ function App() {
   const [isNewGame, setIsNewGame] = useState(false);
   const [isGameOver, setIsGameOver] = useState(false);
   const [cardData, setCardData] = useState([]);
-  const [cards, setCards] = useState([]);
+  const [gameCards, setGameCards] = useState([]);
   const [level, setLevel] = useState(0);
 
   // Start counting levels from 1
   const shownLevel = level + 1;
   // Divide by two because two cards are added each level
-  const topLevel = cards.length / 2;
+  const topLevel = gameCards.length / 2;
   // Shuffle cards at each render
-  const shuffledCards = shuffleArray(cards);
-  const clickedCards = cards.filter((card) => card.isClicked);
+  const shuffledCards = shuffleArray(gameCards);
+  const clickedCards = gameCards.filter((card) => card.isClicked);
 
   useEffect(() => {
     loadCardData().then((cardData) => {
-      setCardData(cardData);
-      setCards(cardData.slice(0, 2));
+      setCardData(shuffleArray(cardData));
+      setGameCards(cardData.slice(0, 2));
       console.log(cardData);
     });
   }, []);
 
   // Check if top level has been reached
   useEffect(() => {
-    if (topLevel === level) {
+    if (checkIfTopLevelAchieved()) {
       gameOver();
     }
   }, [level]);
 
-  // Check if all cards in the level has been clicked
   useEffect(() => {
-    if (cards.length > 0 && clickedCards.length === cards.length) {
+    if (checkIfAllCardsAreClicked()) {
       nextLevel();
     }
-  }, [clickedCards, cards]);
+  }, [clickedCards, gameCards]);
+
+  const checkIfAllCardsAreClicked = () =>
+    gameCards.length > 0 && clickedCards.length === gameCards.length;
+
+  const checkIfTopLevelAchieved = () => topLevel === level;
 
   const playCard = (cardId) => {
     const findCard = clickedCards.find((card) => card.id === cardId);
     if (!findCard) {
-      const playCard = cards.map((card) =>
+      const playCard = gameCards.map((card) =>
         card.id === cardId ? { ...card, isClicked: true } : card
       );
-      setCards(playCard);
+      setGameCards(playCard);
     } else {
       gameOver();
     }
@@ -67,14 +71,14 @@ function App() {
 
   const nextLevel = () => {
     setLevel((prevState) => prevState + 1);
-    setCards((prevState) => cardData.slice(0, prevState.length + 2));
+    setGameCards((prevState) => cardData.slice(0, prevState.length + 2));
     setLevelAnimation();
   };
 
   const gameOver = () => {
     toggleOverlay();
     setIsGameOver((prevState) => !prevState);
-    setCards(cardData.slice(0, 2));
+    setGameCards(cardData.slice(0, 2));
   };
 
   return (
