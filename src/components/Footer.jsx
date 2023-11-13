7494730049f7be3a11ea4b2277bb6f59068f08ca
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2023-11-13 10:17:12 +0200

    Update footer with new github username

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 export function Footer() {
   return (
     <footer className="footer">
-      <h3> Copyright © 2023 TarmoRauta</h3>
+      <h3> Copyright © 2023 aarnif</h3>
     </footer>
   );
 }
