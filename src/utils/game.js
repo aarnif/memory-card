6ad816297c10a38b76291a952e6ddbe116b3c9e2
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2023-11-14 15:59:20 +0200

    Refactor shuffleArray-function

--- src/utils/game.js
+++ src/utils/game.js
@@ -1,5 +1,10 @@
 export const shuffleArray = (array) => {
   const shuffledArray = [...array];
+
+  if (shuffledArray.length <= 1) return shuffledArray;
+
+  if (shuffledArray.length === 2) return [shuffledArray[1], shuffledArray[0]];
+
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
     [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
