638e7e3319304bf7daca0c2ee81389fc3ef02c04
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-01-18 18:20:10 +0200

    Fix custom font url-path and add github-logo to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,19 @@
 import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="footer">
-      <h3> Copyright © 2023 aarnif</h3>
+      <h3>
+        Copyright © {currentYear} aarnif{" "}
+        <FontAwesomeIcon
+          icon={faGithub}
+          style={{ color: "#0077f2" }}
+          size={"lg"}
+        />
+      </h3>
     </footer>
   );
 }
