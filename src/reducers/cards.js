fb37bd490846680b96b5d13a889ec75c3db69152
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-01-19 15:58:30 +0200

    Move all cards related functions into same reducer-file

--- src/reducers/cards.js
+++ src/reducers/cards.js
@@ -1,27 +1,73 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { characters } from "../utils/characterData";
-import { shuffleArray, addNewLevelToCards, resetCards } from "../utils/game";
+
+const resetShownCards = (cards) => {
+  return cards.map((card) => ({ ...card, isShown: false }));
+};
+
+const resetClickedCards = (cards) => {
+  return cards.map((card) => ({ ...card, isClicked: false }));
+};
+
+const addNewCards = (cards, cardsAddedPerLevel) => {
+  let count = 0;
+
+  for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    if (!card.isShown) {
+      card.isShown = true;
+      console.log("Add new card:");
+      console.log(card);
+      ++count;
+    }
+
+    if (count === cardsAddedPerLevel) {
+      break;
+    }
+  }
+  return cards;
+};
+
+const shuffleArray = (array) => {
+  const shuffledArray = [...array];
+
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  return shuffledArray;
+};
 
 const cardsSlice = createSlice({
   name: "cards",
   initialState: [],
   reducers: {
     setCards(state, action) {
-      return action.payload;
+      const cards = action.payload;
+      return cards;
     },
     showCards(state, action) {
-      return addNewLevelToCards(action.payload.cards, action.payload.howMany);
+      const cards = action.payload.cards;
+      const shuffledCards = shuffleArray(cards);
+      const resetClickCards = resetClickedCards(shuffledCards);
+      return addNewCards(resetClickCards, action.payload.howMany);
     },
     reset(state, action) {
-      return resetCards(action.payload.cards, action.payload.howMany);
+      const cards = action.payload.cards;
+      const resetShowCards = resetShownCards(cards);
+      const shuffledCards = shuffleArray(resetShowCards);
+      const resetClickCards = resetClickedCards(shuffledCards);
+      return addNewCards(resetClickCards, action.payload.howMany);
     },
-    shuffle(state, action) {
-      return shuffleArray(action.payload);
+    shuffleCards(state, action) {
+      console.log(action.payload);
+      const cards = action.payload;
+      return shuffleArray(cards);
     },
   },
 });
 
-export const { setCards, showCards, reset, shuffle } = cardsSlice.actions;
+export const { setCards, showCards, reset, shuffleCards } = cardsSlice.actions;
 
 export const iniatilizeCards = () => {
   console.log("Dispatching iniatilizeCards action");
@@ -47,7 +93,7 @@ export const resetDeck = (cards, howMany) => {
 export const shuffleDeck = (cards) => {
   console.log("Dispatching shuffleDeck action");
   return (dispatch) => {
-    dispatch(shuffle(cards));
+    dispatch(shuffleCards(cards));
   };
 };
 
