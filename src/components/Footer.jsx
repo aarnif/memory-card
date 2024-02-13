088e74e56b53a040e3c82421fec9696f7a0de5f3
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-02-13 10:58:35 +0200

    Use tailwind in Footer-component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,23 @@
-import "./Footer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { motion } from "framer-motion";
 
 export function Footer() {
+  const styles = {
+    footer:
+      "bg-black-rgba flex flex-col justify-around items-center shadow-blue-under p-2",
+    ul: "flex justify-center items-center",
+    li: "px-1",
+    h3: "text-xl",
+  };
+
   return (
-    <footer className="footer">
-      <ul className="footer-items">
-        <li>
-          <h3>Created By aarnif</h3>
+    <footer className={styles.footer}>
+      <ul className={styles.ul}>
+        <li className={styles.li}>
+          <h3 className={styles.h3}>Created By aarnif</h3>
         </li>
-        <li>
+        <li className={styles.li}>
           <motion.div
             whileHover={{
               scale: 1.5,
