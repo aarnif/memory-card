119c9913b5b823cfa8b516ced886ea217c436388
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-12-28 15:49:18 +0200

    Update style in Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,26 +3,18 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { motion } from "framer-motion";
 
 export function Footer({ animationTransition }) {
-  const styles = {
-    footer:
-      "bg-header flex flex-col justify-around items-center shadow-blue p-2",
-    ul: "flex justify-center items-center",
-    li: "px-1",
-    h3: "text-xl",
-  };
-
   return (
     <motion.footer
-      className={styles.footer}
+      className="flex flex-col justify-around items-center bg-header shadow-blue p-2"
       initial={{ y: 100 }}
       animate={{ y: 0 }}
       transition={{ ...animationTransition, duration: 0.3 }}
     >
-      <ul className={styles.ul}>
-        <li className={styles.li}>
-          <h3 className={styles.h3}>Created By aarnif</h3>
+      <ul className="flex justify-center items-center">
+        <li className="px-1">
+          <h3 className="text-md lg:text-lg xl:text-xl">Created By aarnif</h3>
         </li>
-        <li className={styles.li}>
+        <li className="px-1">
           <motion.div
             whileHover={{
               scale: 1.5,
